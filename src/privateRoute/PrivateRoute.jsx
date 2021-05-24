e7c674db779bb30f78ab5f9bcc3da6b6a7d19bad
Zeljko Ivanovic <zeljkoivanovic369@gmail.com>
2021-05-24 22:54:41 +0200

    Update PrivateRoute.jsx

--- src/privateRoute/PrivateRoute.jsx
+++ src/privateRoute/PrivateRoute.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import {Route,useHistory} from 'react-router-dom';
+import {Route,useHistory,useLocation} from 'react-router-dom';
 import Forbidden from "../pages/forbidden/Forbidden";
 import AuthLayout from "../components/layout/AuthLayout";
 import BasicLayout from "../components/layout/BasicLayout";
 import {loadFromStorage} from "../functions/tools";
 
-
 const PrivateRoute = ({component: Component, isPrivate, ...rest}) => {
     const Layout = isPrivate ? AuthLayout : BasicLayout;
     const history  = useHistory();
+	const location = useLocation();
+	
     if(loadFromStorage('role') && !isPrivate)history.push('/');
+	if(!loadFromStorage('role') && isPrivate && location.pathname === '/')history.push('/login');
 
     return <Route {...rest} component={() => {
         if(isPrivate && loadFromStorage('role')) return <Layout><Component {...rest}/></Layout>;
